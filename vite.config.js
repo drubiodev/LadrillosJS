83540cec876430d4564d245fd2e78b8da661066d
Daniel Rubio <drubio432@gmail.com>
2025-04-06 19:34:56 -0400

    update root path for samples page

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,13 @@ const path = require("path");
 const { defineConfig } = require("vite");
 
 module.exports = defineConfig({
+  root: path.resolve(__dirname, "samples"),
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/core/main.js"),
       name: "ladrillosjs",
       fileName: (format) => `ladrillosjs.${format}.js`,
     },
+    outDir: path.resolve(__dirname, "dist"),
   },
 });
