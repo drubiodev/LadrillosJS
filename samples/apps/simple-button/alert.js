c4291eb24b76f6235a3f311c46a0cd3e791bfdf8
Daniel Rubio <drubio432@gmail.com>
2025-04-20 11:53:31 -0400

    update alert sample to show seperating js file

--- samples/apps/simple-button/alert.js
+++ samples/apps/simple-button/alert.js
@@ -1,1 +1,16 @@
-alert("HI");
+const title = "Clicked";
+const count = 0;
+
+const p = this.querySelector("p");
+
+function updateName() {
+  title = this.value;
+}
+
+const increaseCount = () => {
+  count++;
+
+  if (count >= 10) {
+    p.textContent = "to many clicks";
+  }
+};
