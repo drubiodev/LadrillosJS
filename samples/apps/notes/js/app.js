f8808986364b37cbf9bd755002826cbf335beb83
Daniel Rubio <drubio432@gmail.com>
2025-05-03 00:10:09 -0400

    update to use this

--- samples/apps/notes/js/app.js
+++ samples/apps/notes/js/app.js
@@ -1,11 +1,9 @@
 import { addNote, notesStore } from "../stores/notesStore.js";
 
 export default function () {
-  const save = () => {
+  this.save = () => {
     const { title, note } = this.state;
     addNote({ title, note });
     this.setState({ title: "", note: "" });
   };
-
-  this.setState({ save });
 }
