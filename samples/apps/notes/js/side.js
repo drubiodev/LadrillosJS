c30ce4ee26100dfaeff81b86b1b565fac980ad72
Daniel Rubio <drubio432@gmail.com>
2025-05-03 00:19:19 -0400

    clean up

--- samples/apps/notes/js/side.js
+++ samples/apps/notes/js/side.js
@@ -3,10 +3,7 @@ import { notesStore } from "../stores/notesStore.js";
 export default function () {
   const ul = this.querySelector("ul");
 
-  // whenever the store changes, rewrite the UL’s innerHTML
-  const unsubscribe = notesStore.subscribe(({ notes }) => {
+  notesStore.subscribe(({ notes }) => {
     ul.innerHTML = notes.map((n) => `<li>${n.title}</li>`).join("");
   });
-
-  // remember to call unsubscribe() in disconnectedCallback if you tear down
 }
