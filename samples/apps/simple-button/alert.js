697408db56586855f186b3a8b0cf2e57512bbdfe
Daniel Rubio <drubio432@gmail.com>
2025-05-04 22:09:42 -0400

    update sample

--- samples/apps/simple-button/alert.js
+++ samples/apps/simple-button/alert.js
@@ -1,17 +1,5 @@
-let count = 0;
-
-// function updateName() {
-//   const title = this.querySelector("input").value;
-//   console.log(title);
-//   this.setState({ title });
-// }
-
-this.p = this.querySelector("p");
+const count = 0;
 
 const increaseCount = () => {
   this.setState({ count: this.state.count + 1 });
-
-  if (this.state.count >= 10) {
-    this.p.textContent = "too many clicks";
-  }
 };
