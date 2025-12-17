5a1376b6a144fee38c25069db271c77c073d5cec
Daniel Rubio <drubio432@gmail.com>
2025-12-17 10:17:47 -0500

    removed unused items

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,5 @@
 import { ladrillos } from "./core/ladrillos";
 
-// Export module executor utilities for advanced use cases
-export {
-  rewriteImports,
-  executeModuleScript,
-  executeAllModuleScripts,
-  cleanupModuleScripts,
-  extractImportSpecifiers,
-  executeModuleScriptWithReactivity,
-  executeModuleScriptsWithReactivity,
-} from "./core/js/moduleExecutor";
-
-// Export directive utilities
-export {
-  scanDirectives,
-  renderLoops,
-  updateConditionals,
-  updateShowElements,
-  setupTwoWayBindings,
-} from "./core/directives/directiveProcessor";
-
-// Export directive constants
-export * from "./utils/directives";
-
 export const registerComponent = (
   name: string,
   path: string,
