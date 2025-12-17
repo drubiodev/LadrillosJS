4777a992332714ab58226ded06dbca281b1cf61c
Daniel Rubio <drubio432@gmail.com>
2025-12-16 23:21:59 -0500

    update sample

--- samples/vite-sample/src/main.ts
+++ samples/vite-sample/src/main.ts
@@ -1,28 +1,25 @@
 import "./style.css";
-import typescriptLogo from "./typescript.svg";
 import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 import { registerComponent } from "ladrillosjs";
 
-registerComponent("my-component", "../components/counter.html");
+registerComponent("my-component", "../components/counter.html",false);
+
+  const lSrc = "https://raw.githubusercontent.com/drubiodev/LadrillosJS/refs/heads/main/LadrillosJS.jpg"
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    <a href="https://drubiodev.github.io/ladrillosjs-site/" target="_blank">
+      <img src="${lSrc}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Vite + LadrillosJS</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
-      <my-component count="5"></my-component>
+      <my-component count="0"></my-component>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click on the Vite and LadrillosJS logos to learn more
     </p>
   </div>
 `;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
